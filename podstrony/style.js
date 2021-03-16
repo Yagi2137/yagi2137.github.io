29b958422bbfb78dce111e7a5e5d8665d3436c1e
Yagi2137 <79875011+Yagi2137@users.noreply.github.com>
2021-03-16 23:26:00 +0100

    up

--- podstrony/style.js
+++ podstrony/style.js
@@ -4,9 +4,13 @@ $(document).ready(function(){
     $("#startMenu").click(function(){
         $("#snap").animate({
          opacity:-0.3,
-         height: 400
+         height: 400,
         }, 1500);
          start.play();
          $("#contentMenu").fadeIn(1500);
+         $("#startMenu").hide(1500);
     });
+    $(".contactEmail").click(function(){
+        alert("kubakszkola123@gmail.com");
+      });
 });
