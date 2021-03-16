ec36f7e4241f66aa92812289386851ac45f73c9b
Yagi2137 <79875011+Yagi2137@users.noreply.github.com>
2021-03-16 18:10:47 +0100

    update

--- podstrony/style.js
+++ podstrony/style.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
     var start = $("#start")[0];
+    $("#contentMenu").hide();
     $("#startMenu").click(function(){
         $("#snap").animate({
-         height:0,
-         opacity:-0.3
-         }, 1500);
+         opacity:-0.3,
+         height: 400
+        }, 1500);
          start.play();
+         $("#contentMenu").fadeIn(1500);
     });
 });
