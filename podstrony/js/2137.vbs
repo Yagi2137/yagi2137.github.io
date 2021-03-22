3857682c9f55151a5c0dc734a3e27a09289ca935
Yagi2137 <79875011+Yagi2137@users.noreply.github.com>
2021-03-22 18:24:28 +0100

    update

---
+++ podstrony/js/2137.vbs
@@ -0,0 +1,1 @@
+x=msgbox("Mozna powiedziec, ze to jest, jakby to powiedziec, okrutnik no po prostu", 0+16, "2137")
---
+++ podstrony/js/script2.js
@@ -0,0 +1,23 @@
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var docu = document.getElementById("xyz");
+    var button = document.getElementById("button");
+    var width = 0;
+    var id = setInterval(frame, 30);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width  + "%";
+        docu.style.cursor = "wait";
+        button.style.cursor = "wait";
+      }
+    }
+  }
+}
